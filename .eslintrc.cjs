8e85104082477e44edcdf0f970f37e7dceb5ee46
sohho888 <sohho888@email.ru>
2024-07-09 13:59:40 +0100

    add and change depends

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,20 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "react-compiler"],
+
+  plugins: [
+    "react-refresh",
+    "react-compiler",
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "prettier",
+  ],
   rules: {
     "react-compiler/react-compiler": "error",
     "react-refresh/only-export-components": [
